a8481a5e319dc421d2140d6dab1cc96e56e5805e
Yousaf Nazir <yousaf.nazir2023@gmail.com>
2024-05-13 23:23:13 +0500

    new commit

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ movingButton.addEventListener('mouseover', () => {
   movingButton.style.top = newTop + 'px';
   movingButton.style.left = newLeft + 'px';
 });
+
+movingButton.addEventListener('touchstart', (event) => {
+  const buttonWidth = movingButton.offsetWidth;
+  const buttonHeight = movingButton.offsetHeight;
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  const newTop = Math.random() * (windowHeight - buttonHeight);
+  const newLeft = Math.random() * (windowWidth - buttonWidth);
+
+  movingButton.style.top = newTop + 'px';
+  movingButton.style.left = newLeft + 'px';
+  
+  event.preventDefault(); // Prevent default touch event behavior
+});
+
 document.getElementById("love-button").addEventListener("click", function() {
   alert("I love you more");
 });
